670aff8fab159b5ad4d676ccb4a73171f6bf365e
PlatonKynin <kininplaton@gmail.com>
2024-02-04 20:25:08 +0300

    9. Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import {Header} from "./layout/header/Header";
 import {Main} from "./layout/section/main/Main";
 import {Skills} from "./layout/section/skills/Skills";
 import {Works} from "./layout/section/works/Works";
+import {Testimony} from "./layout/section/testimony/Testimony";
+import {Contact} from "./layout/section/contacts/Contact";
+import {Slogan} from "./layout/section/slogan/Slogan";
+import {Footer} from "./layout/footer/Footer";
 
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
             <Main/>
             <Skills/>
             <Works/>
+            <Testimony/>
+            <Contact/>
+            <Slogan/>
+            <Footer/>
         </div>
     );
 }
