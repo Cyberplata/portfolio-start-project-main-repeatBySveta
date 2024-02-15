781fc943911c6ee703b1e08850ba371e0e9e8f6a
PlatonKynin <kininplaton@gmail.com>
2024-02-16 00:02:29 +0300

    20. Main part 3

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -4,17 +4,19 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Svetlana Dyablo</span></Name>
                         <MainTitle>A Web Developer.</MainTitle>
                     </div>
+
                     <PhotoWrapper>
                         <Photo src={photo}
                                alt=""/> {/*внутри Не парного тега мы не можем ничего создать -> создаём обертку PhotoWrapper*/}
@@ -34,6 +36,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
     
     &::before {  // Создаём нашу рамку на заднем фоне
         content: "";
@@ -45,6 +48,13 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+        
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -17px;
+            left: 20px;
+        }
     }
 `
 
@@ -52,23 +62,34 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 20px;
+    
+    @media ${theme.media.mobile} { // меняем размер картинки, когда размер окна меньше 576px
+        width: 310px;
+        height: 380px;
+    }
 `
 
 const MainTitle = styled.h1`
-    font-size: 27px;
-    font-weight: 400;
+    //font-size: 27px;
+    //font-weight: 400;
+
+    ${font( {weight: 400, Fmax: 27, Fmin: 20, })}
 `
 
 const Name = styled.h2`
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 50px;
-    font-weight: 700;
+    ${font( {family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36, })} // Вместо 3-х строчек, красивый наш мексин
+    
+    //font-family: 'Josefin Sans', sans-serif;
+    //font-weight: 700;
+    //font-size: 50px;
     letter-spacing: 0.05em;
     margin: 10px 0;
 
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap;
 
         &::before { // Отображаем линию под тексом
             content: "";
@@ -81,6 +102,10 @@ const Name = styled.h2`
             bottom: 0;
             z-index: -1;
         }
+    }
+    
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
     }
 `
 
