2e5a47b46358b9612170d583d2fd7893a7e0c52d
PlatonKynin <kininplaton@gmail.com>
2024-02-17 10:09:47 +0300

    22. Works part 3

--- src/layout/section/works/tabMenu/TabMenu.tsx
+++ src/layout/section/works/tabMenu/TabMenu.tsx
@@ -18,12 +18,14 @@ export const TabMenu = (props: { menuItems: Array<string> }) => {
 };
 
 const StyledTabMenu = styled.nav`
-    margin-bottom: 40px;
-    
     ul {
         display: flex;
-        gap: 20px;
-        justify-content: center;
+        //gap: 20px;
+        justify-content: space-between;
+        max-width: 352px; // связка для нашего табменю даёт нам еффект отзывчивости-резиновые (подстраиваются под окно браузера)
+        width: 100%;
+        border: 1px solid red;
+        margin: 0 auto 40px; // выровнили горизонтально по центру
     }
 `
 
