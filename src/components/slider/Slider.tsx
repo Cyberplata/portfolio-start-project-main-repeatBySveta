cdfc5f2211bf6c79ac5935910015723640896ad6
PlatonKynin <kininplaton@gmail.com>
2024-02-18 11:47:04 +0300

    28.8. Refactoring Footer.tsx, TabMenu.tsx, Slider.tsx, Button.ts, FlexWrapper.ts, Icon.tsx, Link.ts, Logo.tsx ...

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,70 +1,23 @@
 import React from 'react';
-import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper";
-import {theme} from "../../styles/Theme";
+import {S} from "./Slider_Styles";
 
-export const Slider = () => {
+export const Slider: React.FC = () => {
     return (
-        <StyledSlider>
+        <S.Slider>
             <FlexWrapper>
-                <Slide>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                <S.Slide>
+                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.</Text>
-                    <Name>@ivan ivanow</Name>
-                </Slide>
+                        elit.</S.Text>
+                    <S.Name>@ivan ivanow</S.Name>
+                </S.Slide>
             </FlexWrapper>
-            <Pagination>
+            <S.Pagination>
                 <span> </span>
                 <span className={"active"}> </span>
                 <span> </span>
-            </Pagination>
-        </StyledSlider>
+            </S.Pagination>
+        </S.Slider>
     );
 };
-
-const StyledSlider = styled.div`
-    //border: 1px solid red;
-    max-width: 500px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-
-const Slide = styled.div`
-    text-align: center;
-`
-
-const Text = styled.p`
-
-`
-
-const Name = styled.span`
-    color: rgb(255, 255, 255);
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 16px;
-    font-weight: 600;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    margin: 22px 0 32px; // Pixel perfect
-    display: inline-block;
-`
-
-const Pagination = styled.div`
-    span {
-        display: inline-block;
-        width: 7px;
-        height: 7px;
-        border-radius: 20px;
-        background-color: rgba(255, 255, 255, 0.5);
-        
-        & + span {  // Убрали по всем краям margin и добавили margin-left только если перед span есть span
-            margin-left: 5px;
-        }
-        
-        &.active {  // Наш средний span
-            background-color: ${theme.colors.accent};
-            width: 20px;
-        }
-    }
-`
