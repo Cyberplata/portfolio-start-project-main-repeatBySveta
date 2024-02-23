3423eae2456fc117f6a344bc2f7d4f6c2b210790
PlatonKynin <kininplaton@gmail.com>
2024-02-23 22:13:14 +0300

    32. Эффект печатающего текста (Typewriter-effect)

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -3,6 +3,8 @@ import photo from '../../../assets/images/photo.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {S} from "./Main_Styles";
+import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main:React.FC = () => {
     return (
@@ -12,7 +14,18 @@ export const Main:React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name>I am <span>Svetlana Dyablo</span></S.Name>
-                        <S.MainTitle>A Web Developer.</S.MainTitle>
+                        <S.MainTitle>
+                            <p>A Web Developer.</p>
+                            <Typewriter
+                                options={{
+                                    strings: ['A Web Developer', 'A Frontend Developer', 'A Web Designer'],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 150
+                                }}
+                            />
+                        </S.MainTitle>
+
                     </div>
 
                     <S.PhotoWrapper>
